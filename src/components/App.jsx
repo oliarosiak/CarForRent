4d9b267fcb5505223c847822855ed55557f84e84
Olia Rosiak <olia.rosiak@gmail.com>
2023-10-07 23:16:59 +0300

    upd App.js (remove style file), add routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,19 @@
-import { Container } from './App.styled';
+import { lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import SharedLayout from './SharedLayout.jsx';
 
-import CarsList from './carsList/CarsList';
+const MainPage = lazy(() => import('../pages/MainPage.jsx'));
+const CatalogPage = lazy(() => import('../pages/CatalogPage.jsx'));
+const FavoritePage = lazy(() => import('../pages/FavoritePage.jsx'));
 
 export const App = () => {
   return (
-    <Container>
-      <CarsList />
-    </Container>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<MainPage />} />
+        <Route path="catalog" element={<CatalogPage />} />
+        <Route path="favorites" element={<FavoritePage />} />
+      </Route>
+    </Routes>
   );
 };
