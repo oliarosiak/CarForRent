ed8a783782d887a00eb7eab56effdb80010e97f7
Olia Rosiak <olia.rosiak@gmail.com>
2023-10-07 17:37:53 +0300

    create car list & car item files (draft)

---
+++ src/components/carItem/CarItem.jsx
@@ -0,0 +1,19 @@
+const CarItem = () => {
+  return (
+    <div>
+      <img src="" alt="car"></img>
+      <div>
+        <h2>Hyundai, 2007</h2>
+        <p>$25</p>
+      </div>
+      <div>
+        <span>Zaporizhzhia</span> | <span>Ukraine</span> |{' '}
+        <span>Economy Car Rentals</span> | <span>SUV</span> |{' '}
+        <span>Tucson</span> | <span>9598</span> | <span>Apple CarPlay</span>
+      </div>
+      <button>Learn more</button>
+    </div>
+  );
+};
+
+export default CarItem;
---
+++ src/components/carsList/CarsList.jsx
@@ -0,0 +1,13 @@
+import CarItem from 'components/carItem/CarItem';
+
+const CarsList = () => {
+  return (
+    <ul>
+      <li>
+        <CarItem />
+      </li>
+    </ul>
+  );
+};
+
+export default CarsList;
