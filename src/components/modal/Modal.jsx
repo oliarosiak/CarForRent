744b77216c08294c33c56cbdc3eba5ec61f7c5b8
Olia Rosiak <olia.rosiak@gmail.com>
2023-10-08 14:01:07 +0300

    add modal file with style (draft)

---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,48 @@
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+
+import PopUpCard from 'components/popUpCard/PopUpCard';
+import {
+  Overlay,
+  CarInfoCard,
+  CloseButton,
+  CloseButtonImg,
+} from './Modal.styled';
+import closeButton from '../../images/close-x.svg';
+
+const modalRoot = document.querySelector('#modal-root');
+
+const Modal = ({ onCloseModal, info }) => {
+  const overlayClickHandler = event => {
+    if (event.currentTarget === event.target) {
+      onCloseModal();
+    }
+  };
+
+  useEffect(() => {
+    const escKeyHandler = event => {
+      if (event.code === 'Escape') {
+        onCloseModal();
+      }
+    };
+
+    window.addEventListener('keydown', escKeyHandler);
+    return () => {
+      window.removeEventListener('keydown', escKeyHandler);
+    };
+  }, [onCloseModal]);
+
+  return createPortal(
+    <Overlay onClick={overlayClickHandler}>
+      <CarInfoCard>
+        <CloseButton onClick={() => onCloseModal()}>
+          <CloseButtonImg src={closeButton} alt="close button" />
+        </CloseButton>
+        <PopUpCard />
+      </CarInfoCard>
+    </Overlay>,
+    modalRoot
+  );
+};
+
+export default Modal;
---
+++ src/components/modal/Modal.styled.js
@@ -0,0 +1,36 @@
+import styled from '@emotion/styled';
+
+export const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.8);
+  z-index: 1200;
+`;
+
+export const CarInfoCard = styled.div`
+  position: relative;
+  width: 541px;
+  padding: 40px;
+  background-color: #ffffff;
+  border-radius: 24px;
+`;
+
+export const CloseButton = styled.button`
+  position: absolute;
+  top: 16px;
+  right: 16px;
+  padding: 0;
+  border: none;
+`;
+
+export const CloseButtonImg = styled.img`
+  width: 24px;
+  height: 24px;
+  background: #ffffff;
+`;
