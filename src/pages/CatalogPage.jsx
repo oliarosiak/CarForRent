4b77f2984108d1e9a68b7d71089a5dbdfd81c469
Olia Rosiak <olia.rosiak@gmail.com>
2023-10-08 20:48:59 +0300

    Update CatalogPage.jsx

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -2,20 +2,36 @@ import { carsAPI } from '../api/carsAPI';
 import { useState, useEffect } from 'react';
 
 import CarsList from 'components/carsList/CarsList';
+import LoadMoreBtn from 'components/loadMoreBtn/LoadMoreBtn';
 
 const CatalogPage = () => {
   const [cars, setCars] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isActiveBtn, setIsActiveBtn] = useState(true);
 
   useEffect(() => {
+    const CARDS_PER_PAGE = 8;
+
     carsAPI().then(data => {
-      setCars(data);
+      const perPage = CARDS_PER_PAGE * page;
+      const offers = data.slice(0, perPage);
+
+      if (data.length < perPage) {
+        setIsActiveBtn(false);
+      }
+
+      setCars(offers);
     });
-  }, []);
+  }, [page]);
+
+  const loadMoreBtn = () => {
+    setPage(page + 1);
+  };
 
   return (
     <div>
       <CarsList cars={cars} />
-      <button>Load more</button>
+      <LoadMoreBtn handler={loadMoreBtn} visible={isActiveBtn} />
     </div>
   );
 };
