b98654c25e2d0fb6f099a0037f8c276946407523
Olia Rosiak <olia.rosiak@gmail.com>
2023-10-08 14:00:44 +0300

    add draft of popUpCard and style file

---
+++ src/components/popUpCard/PopUpCard.jsx
@@ -0,0 +1,28 @@
+const PopUpCard = () => {
+  return (
+    <>
+      <img src="" alt="car" />
+      <h2>Buick Enclave, 2008</h2>
+      <span>Kiev | Ukraine | Id: 9582 | Year: 2008 | Type: Suv</span>
+      <span>Fuel Consumption: 10.5 | Engine Size: 3.6L V6</span>
+      <p>
+        The Buick Enclave is a stylish and spacious SUV known for its
+        comfortable ride and luxurious features.
+      </p>
+      <h3>Accessories and functionalities:</h3>
+      <span>Leather seats | Panoramic sunroof | Power liftgate</span>
+      <span>Premium audio system | Remote start | Blind-spot monitoring</span>
+      <h3>Rental Conditions: </h3>
+      <div>
+        <p>Minimum age : 25</p>
+        <p>Valid driver’s license</p>
+        <p>Security deposite required </p>
+        <p>Mileage: 5,858</p>
+        <p>Price: 40$</p>
+      </div>
+      <button>Rental car</button>
+    </>
+  );
+};
+
+export default PopUpCard;
