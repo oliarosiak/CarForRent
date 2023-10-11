260b2c9467a2b362ed44514931e46250d2ee9611
Olia Rosiak <olia.rosiak@gmail.com>
2023-10-11 22:38:38 +0300

    Update MainPage.jsx

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,7 @@
-import coverPic from '../images/rent-a-car.jpg';
+import HomeScreen from 'components/homeScreen/HomeScreen';
 
 const MainPage = () => {
-  return (
-    <div>
-      <h1> Car For Rent</h1>
-      <img src={coverPic} alt="cover car" />
-    </div>
-  );
+  return <HomeScreen />;
 };
 
 export default MainPage;
