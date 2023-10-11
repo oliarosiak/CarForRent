460e1718a88c7dd7bb344da90df9659f7600798d
Olia Rosiak <olia.rosiak@gmail.com>
2023-10-11 22:46:38 +0300

    add EmptyFavorite & styles

---
+++ src/components/favorite/EmptyFavorite.jsx
@@ -0,0 +1,11 @@
+import { Wrapper, Text } from './EmptyFavorite.styled';
+
+const EmptyFavorite = () => {
+  return (
+    <Wrapper>
+      <Text>Sorry, you haven't favorite adverts yet</Text>
+    </Wrapper>
+  );
+};
+
+export default EmptyFavorite;
---
+++ src/components/favorite/EmptyFavorite.styled.js
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  width: 600px;
+  margin: 0 auto;
+  padding: 100px 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  border-radius: 12px;
+  background: linear-gradient(to right, #e2e2e2, #c9d6ff);
+`;
+
+export const Text = styled.p`
+  color: #ffffff;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 1.5;
+  text-decoration: underline;
+`;
