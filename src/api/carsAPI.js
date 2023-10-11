ab4a22bad49ceedc2a78f2e8374a037fc019b819
Olia Rosiak <olia.rosiak@gmail.com>
2023-10-11 21:51:49 +0300

    upd api, package

--- src/api/carsAPI.js
+++ src/api/carsAPI.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://65215f44a4199548356d2007.mockapi.io/cars';
-const defaultSettings = '/adverts';
+axios.defaults.baseURL = 'https://65215f44a4199548356d2007.mockapi.io/cars';
 
 export async function carsAPI() {
   try {
-    const { data } = await axios.get(`${BASE_URL}${defaultSettings}`);    
+    const { data } = await axios.get('/adverts');
     return data;
   } catch (error) {
     console.error(error);
