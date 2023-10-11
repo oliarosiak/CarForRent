c9d7bd0840572d6d98b3b1d3d810757b576aba6e
Olia Rosiak <olia.rosiak@gmail.com>
2023-10-11 23:29:37 +0300

    Create EmptyFilterList.jsx

---
+++ src/components/favorite/EmptyFilterList.jsx
@@ -0,0 +1,10 @@
+import { Wrapper, Text } from './EmptyFavorite.styled';
+const EmptyFilterList = () => {
+  return (
+    <Wrapper>
+      <Text>Sorry, no matches found. Try another query</Text>
+    </Wrapper>
+  );
+};
+
+export default EmptyFilterList;
