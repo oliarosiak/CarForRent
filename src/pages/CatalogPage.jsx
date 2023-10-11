17e550a2b62d924b12bcb5ebd3d8cc92403fe1da
Olia Rosiak <olia.rosiak@gmail.com>
2023-10-11 21:52:27 +0300

    Update CatalogPage.jsx

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -5,35 +5,47 @@ import FilterForm from 'components/filterForm/FilterForm';
 import CarsList from 'components/carsList/CarsList';
 import LoadMoreBtn from 'components/loadMoreBtn/LoadMoreBtn';
 
+const CARDS_PER_PAGE = 8;
+
 const CatalogPage = () => {
   const [cars, setCars] = useState([]);
   const [page, setPage] = useState(1);
-  const [isActiveBtn, setIsActiveBtn] = useState(true);
 
   useEffect(() => {
-    const CARDS_PER_PAGE = 8;
-
     carsAPI().then(data => {
-      const perPage = CARDS_PER_PAGE * page;
-      const offers = data.slice(0, perPage);
-
-      if (data.length < perPage) {
-        setIsActiveBtn(false);
-      }
-
-      setCars(offers);
+      setCars(data);
     });
-  }, [page]);
+
+    console.log('first');
+  }, []);
 
   const loadMoreBtn = () => {
     setPage(page + 1);
   };
 
+  const perPage = CARDS_PER_PAGE * page;
+  const advertsPerPage = cars.slice(0, perPage);
+
+  const filterHandler = ({ brand, price, min, max }) => {
+    const findBrand = cars
+      .filter(all => all.make === brand.value)
+      .filter(all => {
+        const onlyPrice = all.rentalPrice.slice(1, all.rentalPrice.length);
+        return onlyPrice === price.value;
+      })
+      .filter(all => all.mileage >= min.value && all.mileage <= max.value);
+    setCars(findBrand);
+  };
+
   return (
     <div>
-      <FilterForm />
-      <CarsList cars={cars} />
-      <LoadMoreBtn handler={loadMoreBtn} visible={isActiveBtn} />
+      <FilterForm onSubmit={filterHandler} />
+      <CarsList cars={advertsPerPage} />
+      {advertsPerPage.length >= perPage ? (
+        <LoadMoreBtn handler={loadMoreBtn} visible={true} />
+      ) : (
+        <LoadMoreBtn handler={loadMoreBtn} visible={false} />
+      )}
     </div>
   );
 };
