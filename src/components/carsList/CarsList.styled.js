ba188abd1aa3df36cc08025159ba5624b7ef0449
Olia Rosiak <olia.rosiak@gmail.com>
2023-12-01 19:24:03 +0200

    Update CarsList.styled.js

--- src/components/carsList/CarsList.styled.js
+++ src/components/carsList/CarsList.styled.js
@@ -1,10 +1,21 @@
 import styled from '@emotion/styled';
 
-export const CarsListWrapper = styled.ul`
-  width: 1184px;
+export const CarsListWrapper = styled.ul` 
   display: flex;
   flex-wrap: wrap;
   column-gap: 29px;
-  margin: 0 auto 50px;
   justify-content: center;
+
+  @media screen and (min-width: 320px) {
+    width: 300px;
+    margin: 0 auto 10px;
+  }
+  @media screen and (min-width: 890px) {
+    width: 880px;
+    margin: 0 auto 15px;
+  }
+  @media screen and (min-width: 1440px) {
+    width: 1184px;
+    margin: 0 auto 50px;
+  }
 `;
