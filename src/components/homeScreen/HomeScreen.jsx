89fe425968b0a26972d537d75f602465637b1f13
Olia Rosiak <olia.rosiak@gmail.com>
2023-12-01 20:30:06 +0200

    upd HomeScreen

--- src/components/homeScreen/HomeScreen.jsx
+++ src/components/homeScreen/HomeScreen.jsx
@@ -4,6 +4,7 @@ import {
   CoverImg,
   Title,
   Text,
+  LinkToCatalog
 } from './HomeScreen.styled';
 
 import coverPic from '../../images/rent-a-car.jpg';
@@ -17,6 +18,7 @@ const HomeScreen = () => {
         <Text>We have cars for every taste and wallet!</Text>
         <Text>&#8226; Quickly and qualitatively</Text>
         <Text>&#8226; At an acceptable price</Text>
+        <LinkToCatalog to="/catalog">Choose auto</LinkToCatalog>
       </TextWrapper>
       <CoverImg src={coverPic} alt="cover car" />
     </Container>
--- src/components/homeScreen/HomeScreen.styled.js
+++ src/components/homeScreen/HomeScreen.styled.js
@@ -1,11 +1,24 @@
 import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
-  width: 1184px;
   margin: 0 auto;
   display: flex;
-  flex-direction: row;
-  justify-content: space-around;
+  /* flex-direction: row;
+  justify-content: space-around; */
+
+  @media screen and (min-width: 320px) {
+    width: 300px;
+    flex-direction: column;
+  }
+  @media screen and (min-width: 890px) {
+    width: 880px;
+  }
+  @media screen and (min-width: 1440px) {
+    width: 1184px;
+    flex-direction: row;
+    justify-content: space-around;
+  }
 `;
 
 export const CoverImg = styled.img`
@@ -13,20 +26,35 @@ export const CoverImg = styled.img`
 `;
 
 export const TextWrapper = styled.div`
-  width: 600px;
-  padding: 100px 30px;
+  /* width: 600px; */
+  /* padding: 100px 30px; */
   text-align: center;
   background: var(--bcg-gradient);
   border-radius: var(--radius-l);
   color: var(--main-accent-color);
   font-weight: 600;
+
+  @media screen and (min-width: 320px) {
+    margin-bottom: 15px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+  }
+  @media screen and (min-width: 890px) {
+    margin-bottom: 20px;
+    padding-top: 15px;
+    padding-bottom: 15px;
+  }
+  @media screen and (min-width: 1440px) {
+    margin-bottom: 0;
+    margin-right: 15px;
+    padding: 100px 30px;
+  }
 `;
 
 export const Title = styled.h1`
   font-size: 30px;
   line-height: 1.5;
   margin-bottom: 20px;
-  text-decoration: underline;
 `;
 
 export const Text = styled.p`
@@ -37,4 +65,23 @@ export const Text = styled.p`
   color: var(--white-color);
   font-size: 20px;
   line-height: 1.5;
+`;
+
+export const LinkToCatalog = styled(Link)`
+  display: inline-block;
+  padding: 5px 15px;
+  color: var(--main-accent-color);
+  font-family: var(--manrope);
+  cursor: pointer;
+  background-color: var(--filter-list-color);
+  border-radius: var(--radius-m);
+  border: var(--border-line);
+  font-weight: 700;
+  font-size: 22px;
+  line-height: 1.5;
+
+  &:hover {
+    color: var(--second-accent-color);
+    background-color: var(--filter-bcg-color);
+  }
 `;
