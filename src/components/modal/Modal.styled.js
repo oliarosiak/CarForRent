8c2b7a03a6f6ba7135b7fe798f3ab7be6d1ea6f2
Olia Rosiak <olia.rosiak@gmail.com>
2023-12-01 20:33:17 +0200

    Update Modal.styled.js

--- src/components/modal/Modal.styled.js
+++ src/components/modal/Modal.styled.js
@@ -16,10 +16,19 @@ export const Overlay = styled.div`
 export const CarInfoCard = styled.div`
   position: relative;
   box-sizing: border-box;
-  width: 541px;
-  padding: 40px;
   background-color: var(--white-color);
   border-radius: var(--radius-xl);
+
+  @media screen and (min-width: 320px) {
+    width: 260px;
+    padding: 40px 5px 20px 5px;  
+  }
+  @media screen and (min-width: 890px) {
+    width: 541px;
+    padding: 40px;
+  }
+  @media screen and (min-width: 1440px) {
+  }
 `;
 
 export const CloseButton = styled.button`
